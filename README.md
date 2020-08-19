4fee4b2c263dd6970c9df0bb2eefc6559a24a220
pomelott <pomelott@163.com>
2020-08-19 20:41:39 +0800

    upload v1

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # import-esm-directory
-a tool for import a directory based on es module fastly
+
+* require es-module directory when use ts in node environment
+* notice: use with ts in node env !! (ts module can use es-module directly.)
+* if useful, could you please give me a start on github.
+
+## fast use
+
+* directory eg:
+
+```bash
+root
+|——main.ts
+└——router
+    |———index.ts
+    │———user.ts
+    └———message
+         |——— news
+         |     |———list.ts
+         |     └———today
+         |          └———newsToday.ts
+         └———show.ts
+```
+
+* router/index.ts
+
+```ts
+import importEsmDirectory from 'import-esm-directory';
+export default importEsmDirectory(module);
+```
+
+* import the directory in main.ts
+
+```ts
+import routerDirectoryPromise from './router';
+routerDirectoryPromise.then((esmDirectory) => {
+    console.log(esmDirectory);
+    console.log(esmDirectory.user);
+    console.log(esmDirectory.message.news.list);
+    console.log(esmDirectory.message.news.today.newsToday);
+})
+```
---
+++ index.ts
@@ -0,0 +1,51 @@
+import path from 'path';
+import fs from 'fs';
+function getFilePrefix (filename: string): string {
+    let matchRes = filename.match(/.*(?=\.[^\.]+)/);
+    if (matchRes && matchRes.length) {
+        return matchRes[0]
+    }
+    return '';
+}
+function initFilePath (baseDir: string, esModule: any) {
+    let dir: string[] = fs.readdirSync(baseDir);
+    let pro:Array<Promise<boolean>> = [];
+    dir.forEach(async (item: string) => {
+        let targetPath: string = path.resolve(baseDir, item);
+        let stat = fs.statSync(targetPath)
+        let pro = [], filenamePrefix = getFilePrefix(item);
+        if (stat.isDirectory()) {
+            esModule[item] = {}
+            initFilePath(targetPath, esModule[item])
+        } else {
+            // filter the default file
+            if (filenamePrefix !== 'index') {
+                pro.push(
+                    new Promise((resolve) => {
+                        // esmodule with import function must return a promise object
+                        import(targetPath).then((module) => {
+                            esModule[filenamePrefix] = module;
+                            resolve(true);
+                        })
+                    })
+                )
+            }
+        }
+    })
+    return Promise.all(pro)
+}
+async function makeEsModule (rootDir: string) {
+    let esModule = {};
+    await initFilePath(rootDir, esModule)
+    return esModule;
+}
+export default async (module: NodeModule): Promise<object> => {
+    const __ = path.sep;
+    // get the root path
+    const regDir = new RegExp(`(${__}(?:[^${__}]+${__}?)+)(?=${__}[^${__}]+)`);
+    let directory = module.filename.match(regDir);
+    if (directory && directory[0]) {
+        return await makeEsModule(directory[0]);
+    }
+    return {}
+}
